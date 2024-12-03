4db9b4404210d8a3892ba0005ffc2d5cfa837389
akihiru <akihiru025@gmail.com>
2024-12-03 22:04:26 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 let data = {
