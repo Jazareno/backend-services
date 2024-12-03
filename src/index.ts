c147284af275fd7311f5ff732f57601320dc04f8
akihiru <akihiru025@gmail.com>
2024-12-03 22:17:42 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 
@@ -7,6 +7,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 let data = {
+  controller: {
+    waterPump: false,
+    fertilizer: false,
+  },
   soilMoisture: "",
   waterTankLevel: "",
   fertilizationLevel: "",
@@ -20,12 +24,53 @@ app.post("/postDeviceState", (request: Request, response: Response) => {
   const { soilMoisture, waterTankLevel, fertilizationLevel } = request.body;
 
   data = {
+    ...data,
     soilMoisture,
     waterTankLevel,
     fertilizationLevel,
   };
   response.status(200).send("ok");
 });
+
+app.post(
+  "/postController/water_pump",
+  (request: Request, response: Response) => {
+    data = {
+      ...data,
+      controller: {
+        ...data.controller,
+        waterPump: !data.controller.waterPump,
+      },
+    };
+    response.status(200).send("ok");
+  }
+);
+
+app.post("/postController/fertilizer", (_: Request, response: Response) => {
+  data = {
+    ...data,
+    controller: {
+      ...data.controller,
+      fertilizer: !data.controller.fertilizer,
+    },
+  };
+  response.status(200).send("ok");
+});
+
+app.post(
+  "/postController/revertState",
+  (request: Request, response: Response) => {
+    data = {
+      ...data,
+      controller: {
+        ...data.controller,
+        fertilizer: false,
+        waterPump: false,
+      },
+    };
+    response.status(200).send("ok");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
